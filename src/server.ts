80de07b924f7e09aee05961613f38621f03b1569
Karla Šmuk <karla.smuk@gmail.com>
2024-11-01 19:21:49 +0100

    added content to pages and style them

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,20 @@ import path from 'path';
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+app.get('/xss', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'xss.html'));
+});
+
+app.get('/sde', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'sde.html'));
 });
 
 app.listen(port, () => {
