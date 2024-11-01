8aace341f89690c5621d0c9d92461ba7e21557db
Karla Šmuk <karla.smuk@gmail.com>
2024-11-01 18:02:58 +0100

    added templates

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript + Node.js + Express!');
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
