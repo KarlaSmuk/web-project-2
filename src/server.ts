87f69fd9d060fd9a313b75d31013bada34447cb7
Karla Šmuk <karla.smuk@gmail.com>
2024-11-01 19:43:54 +0100

    added user table

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import path from 'path';
+import { AppDataSource } from './db';
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
@@ -19,6 +20,10 @@ app.get('/sde', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'sde.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+AppDataSource.initialize()
+    .then(async () => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((error) => console.log(error));
