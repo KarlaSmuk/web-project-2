6f725f3784a3ba6f2fcd59e4e25d87deb4f27e98
Karla Šmuk <karla.smuk@gmail.com>
2024-11-01 21:21:25 +0100

    added form for sign up and endpoint

--- src/server.ts
+++ src/server.ts
@@ -2,23 +2,49 @@ import express from 'express';
 import path from 'path';
 import { AppDataSource } from './db';
 import { seedData } from './seed';
+import bodyParser from 'body-parser';
+import { User } from './entities/User.entity';
+import bcrypt from "bcrypt";
 
 const app = express();
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT;
 
+//rendering views/html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+    res.redirect('home.html');
 });
 
 app.get('/xss', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'xss.html'));
+    res.redirect('xss.html');
 });
 
 app.get('/sde', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'sde.html'));
+    res.redirect('sde.html');
+});
+
+//endpoint for creating user, showing Sensitive Data Exposure
+app.post('/sign-up', async (req, res) => {
+    const { vulnerability, email, password } = req.body;
+
+    const userRepository = AppDataSource.getRepository(User);
+
+    const user = {
+        email: email,
+        password: vulnerability ? password : await bcrypt.hash(password, 10)
+    }
+
+    const createdUser = await userRepository.save(user);
+
+    if (createdUser) {
+        res.redirect('sde.html');
+    } else {
+        res.status(500).json('Error occured')
+    }
+
 });
 
 AppDataSource.initialize()
